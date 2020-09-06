e0535ec76dd76a4f2cb56f09945909b2055e9c15
Akshay Antony <akshaya@valorem.com>
2020-09-06 19:45:07 +0530

    First commit

---
+++ ExpressoApi/Migrations/20200906140036_InitMigration1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ExpressoApi.Migrations
+{
+    public partial class InitMigration1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
